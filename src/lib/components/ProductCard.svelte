7c4f28228f7620c3a0a9f2037319cdffdfc14670
Arijeet10 <arijeetsocial@gmail.com>
2024-06-14 21:52:28 +0530

    added ProductCard component

---
+++ src/lib/components/ProductCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import {goto} from "$app/navigation";
+
+    export let product;
+
+
+</script>
+
+<div class="p-4 h-[50vh] border rounded-md overflow-hidden flex flex-col">
+    <button on:click={()=>goto(`/product/${product.id}`)} class="flex-1 relative">
+        <img src={product.image} alt={product.title} class="absolute inset-0 w-full h-full object-contain"/>
+    </button>
+    <div class=" flex flex-col items-start">
+        <button on:click={()=>goto(`/product/${product.id}`)} class="text-lg font-bold line-clamp-2">{product.title}</button>
+        <p class="text-grey font-medium">${product.price}</p>
+        <button class="px-4 py-2 mt-2 rounded-md font-medium bg-orange hover:bg-orange2 text-white" on:click={() => {}}>Add to Cart</button>
+    </div>
+</div>
--- src/routes/category/[productCategory]/+page.svelte
+++ src/routes/category/[productCategory]/+page.svelte
@@ -1,64 +1,62 @@
 <script lang="ts">
-	import { page } from "$app/stores";
-	import { fetchProducts, fetchProductsByCategory } from '$lib/api';
-	import { products, type Product } from '../../../stores/store';
+  import { page } from "$app/stores";
+  import { fetchProducts, fetchProductsByCategory } from "$lib/api";
+  import ProductCard from "$lib/components/ProductCard.svelte";
+  import { products, type Product } from "../../../stores/store";
 
+  let category: string;
+  let loading = true;
 
-	let category: string;
-	let loading=true;
+  // to watch category value changes
+  $: category = $page.params.productCategory;
 
-	// to watch category value changes
-	$: category = $page.params.productCategory;
+  // to fetch products according to category
+  $: if (category) {
+    fetchProductsByCurrentCategory();
+  }
 
-	// to fetch products according to category
-	$: if (category) {
-		fetchProductsByCurrentCategory();
-	}
-
-
-	// fetch products according category
-	async function fetchProductsByCurrentCategory() {
-		try {
-			loading=true;
-			let productList: Product[];
-			if (category === 'all') {
-				productList = await fetchProducts();
-			} else {
-				productList = await fetchProductsByCategory(category);
-			}
-			products.set(productList);
-		} catch (error) {
-			console.log('Failed to load products:',error);
-		}finally{
-			loading=false;
-		}
-	}
-
-
+  // fetch products according category
+  async function fetchProductsByCurrentCategory() {
+    try {
+      loading = true;
+      let productList: Product[];
+      if (category === "all") {
+        productList = await fetchProducts();
+      } else {
+        productList = await fetchProductsByCategory(category);
+      }
+      products.set(productList);
+    } catch (error) {
+      console.log("Failed to load products:", error);
+    } finally {
+      loading = false;
+    }
+  }
 </script>
 
 {#if loading}
-<div>Loading</div>
+  <div>Loading</div>
 {:else}
-<div class="px-8 py-4 flex flex-col gap-4">
-
+  <div class="px-8 py-4 flex flex-col gap-4">
     <!--Header-->
-	<div class="py-4 font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center">{category=='all'?'All Products':category.split('').map((item,i)=>(i==0?item.toUpperCase():item)).join('')}</div><!--/Header-->
+    <div
+      class="py-4 font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center"
+    >
+      {category == "all"
+        ? "All Products"
+        : category
+            .split("")
+            .map((item, i) => (i == 0 ? item.toUpperCase() : item))
+            .join("")}
+    </div>
+    <!--/Header-->
 
     <!--Products-->
-	<section class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-		{#each $products as product}
-			<div>
-                <img 
-                    src={product.image}
-                    alt=""
-                    class=""
-                />
-            </div>
-		{/each}
-	</section><!--/Products-->
-
-</div>
-
+    <section class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {#each $products as product}
+        <ProductCard {product} />
+      {/each}
+    </section>
+    <!--/Products-->
+  </div>
 {/if}
-
