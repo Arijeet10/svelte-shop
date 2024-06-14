e1a41589fb92531ace6678858ffdd959a07e61e9
Arijeet10 <arijeetsocial@gmail.com>
2024-06-14 22:12:00 +0530

    added product filter component and feature

---
+++ src/lib/components/ProductFilter.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	export let handlePriceFilter: (event: Event) => void;
+	export let handleRatingFilter: (event: Event) => void;
+	export let handleSort: (event: Event) => void;
+</script>
+
+<div class="flex items-center justify-between">
+	<div class="flex items-center gap-4">
+		<div class="flex-1">
+			<select on:change={handlePriceFilter} >
+				<option value="" class="text-grey">Price</option>
+				<option value="100" >Max $100</option>
+				<option value="200" >Max $200</option>
+				<option value="300" >Max $300</option>
+				<option value="400" >Max $400</option>
+				<option value="500" >Max $500</option>
+				<option value="600" >Max $600</option>
+				<option value="700" >Max $700</option>
+				<option value="800" >Max $800</option>
+				<option value="900" >Max $900</option>
+				<option value="1000" >Max $1000</option>
+			</select>
+		</div>
+		<div class="flex-1">
+			<select on:change={handleRatingFilter}>
+				<option value="" class="text-grey">Ratings</option>
+				<option value="4">4 ⭐ & above</option>
+				<option value="3">3 ⭐ & above</option>
+				<option value="2">2 ⭐ & above</option>
+				<option value="1">1 ⭐ & above</option>
+			</select>
+		</div>
+	</div>
+	<div class="flex items-center">
+		<select on:change={handleSort}>
+			<option value="" class="text-grey">Sort</option>
+			<option value="price">By Price</option>
+			<option value="reviews">By Popularity</option>
+		</select>
+	</div>
+</div>
+
+
+
--- src/routes/category/[productCategory]/+page.svelte
+++ src/routes/category/[productCategory]/+page.svelte
@@ -2,8 +2,14 @@
   import { page } from "$app/stores";
   import { fetchProducts, fetchProductsByCategory } from "$lib/api";
   import ProductCard from "$lib/components/ProductCard.svelte";
+  import ProductFilter from "$lib/components/ProductFilter.svelte";
+  import { get } from "svelte/store";
   import { products, type Product } from "../../../stores/store";
 
+  let filteredProducts: Product[] = [];
+  let priceFilter: number = 999;
+  let ratingFilter: number = 0;
+  let sortFilter: string = "";
   let category: string;
   let loading = true;
 
@@ -14,6 +20,50 @@
   $: if (category) {
     fetchProductsByCurrentCategory();
   }
+
+  // function to apply filters
+  function applyFilters() {
+    let allProducts = get(products);
+
+    // price filter
+    if (priceFilter !== Infinity) {
+      allProducts = allProducts.filter((item) => item.price <= priceFilter);
+    }
+
+    // rating filter
+    if (ratingFilter !== 0) {
+      allProducts = allProducts.filter(
+        (item) => item.rating.rate >= ratingFilter
+      );
+    }
+
+    // sort filter
+    if (sortFilter === "price") {
+      allProducts.sort((a, b) => a.price - b.price);
+    } else if (sortFilter === "reviews") {
+      allProducts.sort((a, b) => b.rating.rate - a.rating.rate);
+    }
+
+    filteredProducts = allProducts;
+  }
+
+  // Handle price filter change
+  const handlePriceFilter = (event) => {
+    priceFilter = parseFloat(event.target.value) || 999;
+    applyFilters();
+  };
+
+  // Handle rating filter change
+  const handleRatingFilter = (event) => {
+    ratingFilter = parseFloat(event.target.value) || 0;
+    applyFilters();
+  };
+
+  // Handle sort filter change
+  const handleSort = (event) => {
+    sortFilter = event.target.value;
+    applyFilters();
+  };
 
   // fetch products according category
   async function fetchProductsByCurrentCategory() {
@@ -26,6 +76,7 @@
         productList = await fetchProductsByCategory(category);
       }
       products.set(productList);
+      applyFilters();
     } catch (error) {
       console.log("Failed to load products:", error);
     } finally {
@@ -49,11 +100,12 @@
             .map((item, i) => (i == 0 ? item.toUpperCase() : item))
             .join("")}
     </div>
+    <ProductFilter {handlePriceFilter} {handleRatingFilter} {handleSort} />
     <!--/Header-->
 
     <!--Products-->
     <section class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {#each $products as product}
+      {#each filteredProducts as product}
         <ProductCard {product} />
       {/each}
     </section>
