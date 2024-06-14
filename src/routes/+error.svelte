05bfd21f3da71c9a9ce51fb1d7702b74227adc48
Arijeet10 <arijeetsocial@gmail.com>
2024-06-14 23:16:29 +0530

    added custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import {page} from "$app/stores"
+</script>
+
+<div class="min-h-[100vh] flex items-center justify-center">
+    <div class="flex items-center gap-2 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">
+        <div class="text-orange2">{$page.status}:</div>
+        <div>{$page.error?.message}</div>
+    </div>
+</div>
